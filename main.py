5cb797d90a0f62840bc6fcc045c0ccc3161780f7
JordiEspinozaMendoza <jordi8101@gmail.com>
2023-11-21 05:51:57 +0000

    fix: init ep

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import io
 import sys
 from dotenv import load_dotenv
+import boto3
 
 load_dotenv()
 
@@ -11,6 +12,25 @@ app = Flask(__name__)
 @app.route("/notify/v1/health")
 def health_check():
     return jsonify({"status": "UP"})
+    
+@app.route("/api/image/", methods=["POST"])
+def upload_image():
+    try:
+        image = request.files["file"].read()
+        return jsonify(
+            {
+                "status": "success",
+            }
+        )
+    except Exception as e:
+        error = str(e)
+
+        print(e, sys.exc_info()[-1].tb_lineno)
+
+        return jsonify(
+            {"error": error, "status": "error"}
+        )
+
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

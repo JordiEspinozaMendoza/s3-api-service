6879cac92f95c841a7b13bf93c0758d818fcac79
Jordi <jordi8101@gmail.com>
2023-11-27 22:21:50 -0800

    fix: added cur connection

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import boto3
 import psycopg2
 import json
-import uuid 
+import uuid
 
 load_dotenv()
 
@@ -16,14 +16,22 @@ app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 def get_db_connection():
-    conn = psycopg2.connect(host=os.environ.get('DB_HOST'), database=os.environ.get('DB_DATABASE'), user=os.environ.get('DB_USERNAME'), password=os.environ.get('DB_PASSWORD'))
-    
+    conn = psycopg2.connect(
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_DATABASE"),
+        user=os.environ.get("DB_USERNAME"),
+        password=os.environ.get("DB_PASSWORD"),
+    )
+
     return conn
-    
+
+
 @app.route("/api/notify/v1/health")
 def health_check():
     return jsonify({"status": "UP"})
+
 
 @app.route("/api/image/", methods=["POST"])
 def upload_image():
@@ -33,40 +41,66 @@ def upload_image():
         image = file.read()
         filename = file.filename
         _id = uuid.uuid1()
-        
+
         cur = get_db_connection().cursor()
-        
+
         s3_resource = boto3.resource("s3")
         bucket = s3_resource.Bucket(os.environ.get("BUCKET_NAME"))
-        response = bucket.Object(filename).put(Body=file,Metadata={'socketId': socketId})
+        response = bucket.Object(filename).put(
+            Body=file, Metadata={"socketId": socketId}
+        )
 
-        return jsonify({'message': 'File uploaded successfully'})
-        
+        # Create table images if not exists
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS images (id uuid, filename varchar, socketId varchar, url varchar);"
+        )
+
+        # Insert image into table
+        cur.execute(
+            "INSERT INTO images (id, filename, socketId) VALUES (%s, %s, %s)",
+            (_id, filename, socketId),
+        )
+
+        # Commit changes
+        get_db_connection().commit()
+
+        # Close connection
+        cur.close()
+
+        return jsonify({"message": "File uploaded successfully"})
+
     except Exception as e:
         error = str(e)
 
         print(e, sys.exc_info()[-1].tb_lineno)
 
-        return jsonify(
-            {"error": error, "status": "error"}
-        )
-        
+        return jsonify({"error": error, "status": "error"})
+
+
 @app.route("/api/image/uploaded/", methods=["POST"])
 def uploadedImage():
     object_key = request.json["object_key"]
     s3_resource = boto3.client("s3")
-    
-    response = s3_resource.get_object(Bucket=os.environ.get("BUCKET_NAME"), Key=object_key)
+
+    response = s3_resource.get_object(
+        Bucket=os.environ.get("BUCKET_NAME"), Key=object_key
+    )
     ResponseMetadata = response["ResponseMetadata"]
-    
+
     socketId = ResponseMetadata.get("HTTPHeaders").get("x-amz-meta-socketid")
-    socketio.emit('image_uploaded', {'object_key': object_key, "message": "File uploaded successfully"}, to=socketId)
-    
-    return jsonify({'message': 'Message emmited succesfully'})
-    
-@socketio.on('file_uploaded')
+    socketio.emit(
+        "image_uploaded",
+        {"object_key": object_key, "message": "File uploaded successfully"},
+        to=socketId,
+    )
+
+    return jsonify({"message": "Message emmited succesfully"})
+
+
+@socketio.on("file_uploaded")
 def handleUpload(data):
     print(data)
-    
+
+
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True)

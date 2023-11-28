4ccdddab993c2066183c17d6b5cd9a1b404f0057
JordiEspinozaMendoza <jordi8101@gmail.com>
2023-11-28 05:46:20 +0000

    feat: added socket

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, request
+from flask_socketio import SocketIO
 from flask_cors import CORS
 import os
 import io
 import sys
 from dotenv import load_dotenv
 import boto3
+import json
 
 load_dotenv()
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/api/notify/v1/health")
 def health_check():
@@ -20,12 +22,13 @@ def health_check():
 def upload_image():
     try:
         file = request.files["file"]
+        socketId = request.form.get("socketId")
         image = file.read()
         filename = file.filename
         
         s3_resource = boto3.resource("s3")
         bucket = s3_resource.Bucket(os.environ.get("BUCKET_NAME"))
-        bucket.Object(filename).put(Body=file)
+        response = bucket.Object(filename).put(Body=file,Metadata={'socketId': socketId})
 
         return jsonify({'message': 'File uploaded successfully'})
         
@@ -37,7 +40,23 @@ def upload_image():
         return jsonify(
             {"error": error, "status": "error"}
         )
-
+        
+@app.route("/api/image/uploaded/", methods=["POST"])
+def uploadedImage():
+    object_key = request.json["object_key"]
+    s3_resource = boto3.client("s3")
+    
+    response = s3_resource.get_object(Bucket=os.environ.get("BUCKET_NAME"), Key=object_key)
+    ResponseMetadata = response["ResponseMetadata"]
+    
+    socketId = ResponseMetadata.get("HTTPHeaders").get("x-amz-meta-socketid")
+    socketio.emit('image_uploaded', {'object_key': object_key, "message": "File uploaded successfully"}, to=socketId)
+    
+    return jsonify({'message': 'Message emmited succesfully'})
+    
+@socketio.on('file_uploaded')
+def handleUpload(data):
+    print(data)
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
